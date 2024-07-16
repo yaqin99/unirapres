9703c96e1d4b5799301060a75ed0f6474b9597a6
yaqin99 <yaqingithub@gmail.com>
2024-07-16 08:38:42 +0700

    yak ges

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,6 +13,34 @@ class AuthController extends Controller
             'username' => 'required' , 
             'password' => 'required' , 
         ]); 
+        $curl = curl_init();
+        
+        curl_setopt_array($curl, [
+          CURLOPT_URL => "https://api.unira.ac.id/v1/token",
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => "",
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 30,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => "POST",
+          CURLOPT_POSTFIELDS => "-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"username\”\r\n\r\n”nimmu”\r\n——011000010111000001101001\r\nContent-Disposition: form-data; name=\"password\”\r\n\r\n”password”\r\n——011000010111000001101001--\r\n",
+          CURLOPT_COOKIE => "PHPSESSID=4jejk0dtbvgdc371ovtnvund89",
+          CURLOPT_HTTPHEADER => [
+            "Content-Type: multipart/form-data; boundary=---011000010111000001101001",
+            "User-Agent: insomnia/9.3.2"
+          ],
+        ]);
+        
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+        
+        curl_close($curl);
+        
+        if ($err) {
+          echo "cURL Error #:" . $err;
+        } else {
+          echo $response;
+        }
 
         
         // $nama = User::select('name')->where('username' , $req->username)->first();
